33a16306877c1417294d667804909fae0704b1fe
Yuvraj0732 <yuvrajsaini732@yahoo.com>
2020-08-03 16:24:57 +0530

    New Object Added

--- src/test/java/repository/objects/Home.java
+++ src/test/java/repository/objects/Home.java
@@ -6,4 +6,6 @@ public class Home {
 	
 	
 	public static final By HomeMenu= By.xpath("//a[@id='nav-hamburger-menu']");
+	public static final By txtHello= By.xpath("//div[@id='glow-ingress-block']/span[contains(text(),\"Hello\")]");
+	
 }
